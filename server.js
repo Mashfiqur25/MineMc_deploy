59aa1048a42308a4f82fa15cb2def3b0e7f0b355
Mashfiqur25 <83066761+Mashfiqur25@users.noreply.github.com>
2024-10-29 18:36:21 +0600

    port change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const BotManager = require('./botManager');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 // Serve static files
 app.use(express.static('public'));
